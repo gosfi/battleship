dabcf6efbf4a83977cd643325210b5ded5aa7030
Raphaël St-Louis <rstlouis@isimtl.local>
2019-06-19 14:35:39 -0400

    testAIPlayer

--- TP/src/vue/PartieOffline.java
+++ TP/src/vue/PartieOffline.java
@@ -7,13 +7,21 @@ import java.util.function.Consumer;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-import modele.Shoot;
+import modele.*;
 
 
 public class PartieOffline extends JFrame {
 	PlateauJeu partieOff = new PlateauJeu("jouseur 1",1000,800);
 	public PartieOffline() {
 	partieOff.setVisible(true);
+	boolean tour = true;
+	boolean gameOver = true;
+	do {
+		if()
+		
+		
+		
+	}while(gameOver);
 	
 	
 	
