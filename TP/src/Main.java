a2ecc5b9a321a2f07172ea49e918bb882dc995b9
Samuel Lefrançois <samLef12@outlook.com>
2019-06-19 15:51:09 -0400

    controller

--- TP/src/Main.java
+++ TP/src/Main.java
@@ -1,11 +1,11 @@
 import vue.*;
-
+import controlleur.*;
 public class Main {
     public static void main(String[] args){
-		//YAAAAAAAAAAA YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET  
-
-        Menu menuWindow = new Menu("Menu principal",600,600);
+        Control cont = new Control();
+        Menu menuWindow = new Menu("Menu principal",600,600,cont);
         //PlateauJeu jeu = new PlateauJeu("jouseur 1",1000,800);
         PartieOffline partieOff = new PartieOffline();
+        Rulez ok = new Rulez("rules", 600,600);
     }
 }
--- TP/src/vue/Menu.java
+++ TP/src/vue/Menu.java
@@ -5,33 +5,49 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import controlleur.Control;
 
 @SuppressWarnings("serial")
 public class Menu extends FrameWindow {
-    public Menu(String nom, int width, int height) {
-        super(nom, width, height,2,1,1);
 
+    public Menu(String nom, int width, int height,Control unControleur) {
+        super(nom, width, height,2,1,1);
         String[] noms = {"Vs CPU", "Rules", "Vs another player"};
         addLabel("Sploosh Kaboom",SwingConstants.CENTER,32);
         JPanel panel = this.addPanel();
         panel.setLayout(new GridLayout(3,1));
-
+        EcouteurBoutons1 ecouteur = new EcouteurBoutons1(unControleur);
         for(int i = 0; i <noms.length; i++){
             JPanel panel2 =this.addPanel();
             JButton btn = this.addButton(noms[i],500,75);
-            btn.addActionListener(new EcouteurBoutons1());
+            btn.addActionListener(new EcouteurBoutons1(ecouteur));
             panel2.add(btn);
             panel.add(panel2);
-            setResizable(false);
+
         }
 
         this.validate();
+        setResizable(false);
     }
 
 }
-class EcouteurBoutons1 implements ActionListener {
+class EcouteurBoutons1 extends Control implements ActionListener{
+    Control cont;
+    public EcouteurBoutons1(Control unControleur) {
+        this.cont = unControleur;
+    }
     @Override
-    public void actionPerformed(ActionEvent actionEvent) {
+    public void actionPerformed(ActionEvent e) {
+        JButton btn = (JButton) e.getSource();
+
+        cont.mainMenuAction(btn.getText());
+
+
 
     }
-}
+
+
+
+
+    }
+
