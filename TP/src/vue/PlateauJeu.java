e84220bb245f1ae49b9f350f6ba3886a4227df4c
Samuel Lefrançois <samLef12@outlook.com>
2019-06-20 14:44:56 -0400

    Sup Gamer

--- TP/src/vue/PlateauJeu.java
+++ TP/src/vue/PlateauJeu.java
@@ -8,9 +8,7 @@ import modele.Shoot;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import java.util.Collection;
 
 @SuppressWarnings("serial")
 // une chance j'suis la pour faire toutes le fenetre -Sam 2019
@@ -18,6 +16,7 @@ public class PlateauJeu extends FrameWindow {
 	Control cont;
 	ArrayList<JButton> btnArrayEnemy = new ArrayList<>();
 	ArrayList<JButton> btnArrayPlayer = new ArrayList<>();
+	int[]indice = new int[100];
 
 	public PlateauJeu(String nom, int width, int height) {
 		super(nom, width, height, 1, 3, 1);
@@ -29,10 +28,13 @@ public class PlateauJeu extends FrameWindow {
 		JPanel panel2 = this.addPanel();
 		panel2.setLayout(new GridLayout(10, 10));
 		panel2.setBorder(BorderFactory.createEmptyBorder(120, 120, 120, 120));
+
 		for (int i = 0; i < 100; i++) {
+
 			//YAAAAAAAAAAA YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET
 			btnArrayEnemy.add(this.addButton("" + (i + 1), 60, 60));
 			btnArrayEnemy.get(i).addActionListener(ecouteur);
+			indice[i] = i;
 			panel.add(btnArrayEnemy.get(i));
 			pack();
 		}
@@ -59,6 +61,10 @@ public class PlateauJeu extends FrameWindow {
 	public void finPartieBug(){
 		this.dispose();
 		new Menu("Menu principal",600,600,cont);
+	}
+	public int[] getIndice(){
+
+		return indice;
 	}
 }
 
