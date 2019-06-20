8a58500566b4c25d87916e42d670c440b49ca40a
Marco Orellana <marcoorellanaaravena@gmail.com>
2019-06-20 10:56:01 -0400

    BIG OFF

--- TP/src/modele/IA.java
+++ TP/src/modele/IA.java
@@ -1,28 +1,5 @@
 package modele;
 
-import modele.Shoot;
-import vue.*;
-import java.util.ArrayList;
-import java.util.Random;
-
 public class IA {
 
-	// PlateauJeu plateau;
-	PlateauJeu plateau;
-	Shoot shoot = new Shoot();
-	public boolean a = false;
-
-	public void IAAction() {
-		shoot.actionShoot(this.plateau.getArrayPlayer().get(new Random().nextInt(100)));
-		
-
-	}
-
-	public PlateauJeu getPlateauJeu() {
-		return this.plateau;
-	}
-
-	public void setPlateauJeu(PlateauJeu plateau) {
-		this.plateau = plateau;
-	}
 }
--- TP/src/vue/Menu.java
+++ TP/src/vue/Menu.java
@@ -1,6 +1,5 @@
 package vue;
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,63 +8,49 @@ import controlleur.Control;
 import modele.IA;
 
 @SuppressWarnings("serial")
-public class Menu extends FrameWindow implements ActionListener{
-    Control cont;
+public class Menu extends FrameWindow implements ActionListener {
+	Control cont;
 
+	public Menu(String nom, int width, int height, Control unControleur) {
+		super(nom, width, height, 2, 1, 1);
 
-    public Menu(String nom, int width, int height,Control unControleur) {
-        super(nom, width, height,2,1,1);
+		String[] noms = { "Vs CPU", "Rules", "Vs another player" };
+		addLabel("Sploosh Kaboom", SwingConstants.CENTER, 32);
+		JPanel panel = this.addPanel();
+		panel.setLayout(new GridLayout(3, 1));
+		for (int i = 0; i < noms.length; i++) {
+			JPanel panel2 = this.addPanel();
+			JButton btn = this.addButton(noms[i], 500, 75);
+			btn.addActionListener(this);
+			panel2.add(btn);
+			panel.add(panel2);
 
-        String[] noms = {"Vs CPU", "Rules", "Vs another player"};
-        addLabel("Sploosh Kaboom",SwingConstants.CENTER,32);
-        JPanel panel = this.addPanel();
-        panel.setLayout(new GridLayout(3,1));
-        for(int i = 0; i <noms.length; i++){
-            JPanel panel2 =this.addPanel();
-            JButton btn = this.addButton(noms[i],500,75);
-            btn.addActionListener(this);
-            panel2.add(btn);
-            panel.add(panel2);
+		}
 
-        }
+		this.validate();
+		setResizable(false);
+	}
 
-        this.validate();
-        setResizable(false);
-    }
+	@Override
+	public void actionPerformed(ActionEvent actionEvent) {
 
+		JButton btn = (JButton) actionEvent.getSource();
 
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
+		btn.getText();
 
+		if (btn.getText() == "Vs CPU") {
+			this.dispose();
+			PlateauJeu plat = new PlateauJeu("Jouseur 1", 1000, 800);
 
-        JButton btn = (JButton) actionEvent.getSource();
+		} else if (btn.getText() == "Rules") {
+			this.dispose();
+			Rulez rules = new Rulez("rules", 600, 600);
 
-        btn.getText();
+		} else {
+			this.dispose();
+			PlateauJeu plat = new PlateauJeu("Jouseur 1", 1000, 800);
 
-        if (btn.getText() == "Vs CPU"){
-            this.dispose();
-            PlateauJeu plat= new PlateauJeu("Jouseur 1",1000,800);
-            IA ai = new IA();
-            PartieOffline partieOff = new PartieOffline(ai);
-            ai.setPlateauJeu(plat);
-        }
-        else if(btn.getText() =="Rules" ){
-            this.dispose();
-            Rulez rules = new Rulez("rules", 600,600);
+		}
 
-        }
-        else {
-            this.dispose();
-            PlateauJeu plat = new PlateauJeu("Jouseur 1",1000,800);
-
-        }
-
-    }
+	}
 }
-
-
-
-
-
-
-
