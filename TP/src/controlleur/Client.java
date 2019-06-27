c9bb2acb7148c59f7ce288baddd92360e85a78b0
gosfi <44752770+gosfi@users.noreply.github.com>
2019-06-26 23:42:39 -0400

    Update Client.java

--- TP/src/controlleur/Client.java
+++ TP/src/controlleur/Client.java
@@ -1,4 +1,5 @@
 package controlleur;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,51 +10,49 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-	public static void main(String[] args) {
-		try {
+    public static void main(String[] args) {
+        try {
 
-			System.out.println("CLIENT : tentative");
-			Socket socketClient = new Socket("192.168.60.21", 12345);
-			System.out.println("CLIENT : connecter");
+            System.out.println("CLIENT : tentative");
+            Socket socketClient = new Socket("192.168.60.21", 12345);
+            System.out.println("CLIENT : connecter");
 
-			
-			
-			//comunication avec le serveur
-			
-			InputStream is = socketClient.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
 
-			
+            //comunication avec le serveur
 
-			String ligne = br.readLine();
-			String ligne2 = br.readLine();
+            InputStream is = socketClient.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
 
-			System.out.println("CLIENT : serveur dit " + ligne + " et "+ ligne2);
-			int i = Integer.parseInt(ligne) + Integer.parseInt(ligne2 );
-			System.out.println(i);
-			
-			String conazo = Integer.toString(i);
-			
-			OutputStream os=socketClient.getOutputStream();
-			
-			PrintWriter pr = new PrintWriter(os, true);
-			pr.println(conazo);
-		
-	
-			socketClient.close();
 
-			// fermeture de la connexion
-			System.out.println("CLIENT : connexion fermee.");
+            String ligne = br.readLine();
+            String ligne2 = br.readLine();
 
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+            System.out.println("CLIENT : serveur dit " + ligne + " et " + ligne2);
+            int i = Integer.parseInt(ligne) + Integer.parseInt(ligne2);
+            System.out.println(i);
+
+            String conazo = Integer.toString(i);
+
+            OutputStream os = socketClient.getOutputStream();
+
+            PrintWriter pr = new PrintWriter(os, true);
+            pr.println(conazo);
+
+
+            socketClient.close();
+
+            // fermeture de la connexion
+            System.out.println("CLIENT : connexion fermee.");
+
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
 
